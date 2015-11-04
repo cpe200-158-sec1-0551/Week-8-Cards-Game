8f34785a13cff3ba7345eae2903faec37d5b87d1
KRIANGKRAI KOLIBUT <kriangkai_k@cmu.ac.th>
2015-11-05 01:17:06 +0700

    Week 8 Cards Game
    570610551

---
+++ Cards.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Cards : Program
+    {
+        private int rank;
+        private int suit;
+        public Cards(int rankin , int suitin)
+        {
+            rank = rankin;
+            suit = suitin;
+        }
+
+        public int getRank()
+        {
+            return rank;
+        }
+
+        public string nameRank()
+        {
+            switch(rank)
+            {
+                case 1:return "A-";
+                case 11:return "J-";
+                case 12:return "Q-";
+                case 13:return "K-";
+                default: return Convert.ToString(rank)+"-";
+            }
+        }
+        public string nameSuit()
+        {
+            switch (suit)
+            {
+                case 1: return "Clubs";
+                case 2: return "Dimonds";
+                case 3: return "Hearts";
+                case 4: return "Spades";
+                default: return " ";
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Player : Program
+    {
+        private List<Cards> deck;
+        private List<Cards> pile;
+        private string name;
+
+        public Player(string namein)
+        {
+            name = namein;
+            deck = new List<Cards>();
+            pile = new List<Cards>();
+        }
+
+        public void addCard(Cards cardin)
+        {
+            deck.Add(cardin);
+        }
+
+        public int getNumdeck()
+        {
+            return deck.Count;
+        }
+
+        public Cards Draw()
+        {
+            Cards temp = deck[0];
+            deck.RemoveAt(0);
+            return temp;
+        }
+
+        public void addPile(Cards cardin)
+        {
+            pile.Add(cardin);
+        }
+
+        static List<Cards> ShuffleList(List<Cards> inputList)
+        {
+            List<Cards> randomList = new List<Cards>();
+
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count);
+                randomList.Add(inputList[randomIndex]);
+                inputList.RemoveAt(randomIndex);
+            }
+
+            return randomList;
+        }
+
+        public void deckShuff()
+        {
+            deck = ShuffleList(deck);
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public int getNumPile()
+        {
+            return pile.Count;
+        }
+    }
+}
